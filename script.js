2de664afddbfeb8064e481a09e1cd342f8ee77dd
Caius <ibiapina.caius@gmail.com>
2024-02-05 15:10:53 +0000

    axios.get

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const nome = prompt('Qual seu nome?');
 
-const usuario = [];
+let usuario = [];
+let conteudo = [];
 
 function entradaNaSala() {
     const novoUsuario = {name: nome};
@@ -18,9 +19,54 @@ function cadastrarUsuario() {
 }
 
 function envioComSucesso(sucesso) {
-    console.log('deu certo');
+    console.log('Cadastro deu certo: ' + sucesso.data);
 }
 
 function envioComFalha(falha) {
-    console.log("Status code: " + falha.response.status);
+    console.log("Cadastro com Erro: " + falha.response.status);
 }
+
+const mensagens = axios.get('https://api-bate-papo-caiusidt.onrender.com/messages');
+mensagens.then(recebeuComSucesso);
+mensagens.catch(recebeuComFalha);
+
+function recebeuComSucesso(sucesso) {
+    console.log('Recebeu mensagens com sucesso: ' + sucesso.data);
+    conteudo = sucesso.data;
+    renderizarConteudo();
+}
+
+function recebeuComFalha(erro) {
+    console.log('Falha ao receber mensagem: ' + erro.response.status);
+}
+
+renderizarConteudo(); {
+    const ul = document.querySelector('campo-de-mensagens');
+    ul.innerHTML = '';
+
+    for (let i = 0; i < conteudo.length; i++) {
+        ul.innerHTML = ul.innerHTML + `
+        <li>
+            ${conteudo[i].from}
+        </li>    
+        `
+    }
+}
+
+
+/* function buscarMensagens() {
+    const mensagens = axios.get('https://api-bate-papo-caiusidt.onrender.com/messages');
+    console.log(mensagens);
+    mensagens.then(recebeuComSucesso);
+    mensagens.catch(recebeuComFalha);
+}
+
+function recebeuComSucesso(sucesso) {
+    console.log('Recebeu mensagens com sucesso: ' + sucesso.data);
+}
+
+function recebeuComFalha(erro) {
+    console.log('Falha ao receber mensagem: ' + erro.response.status);
+}
+
+buscarMensagens();*/
