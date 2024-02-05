ba361291f43ecc464c48105ce0ba5e243571949a
Caius <ibiapina.caius@gmail.com>
2024-02-05 02:45:44 +0000

    cadastro usuário e axios.post

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const nome = prompt('Qual seu nome?');
 
+const usuario = [];
+
 function entradaNaSala() {
-    const novoUsuario = nome;
-    console.log(novoUsuario);
+    const novoUsuario = {name: nome};
+    usuario.push(novoUsuario);
+    cadastrarUsuario();
 }
 
+entradaNaSala();
+
+function cadastrarUsuario() {
+    const cadastro = axios.post('https://api-bate-papo-caiusidt.onrender.com/participants', usuario[0]);
+    console.log(cadastro);
+    cadastro.then(envioComSucesso);
+    cadastro.catch(envioComFalha);
+}
+
+function envioComSucesso(sucesso) {
+    console.log('deu certo');
+}
+
+function envioComFalha(falha) {
+    console.log("Status code: " + falha.response.status);
+}
